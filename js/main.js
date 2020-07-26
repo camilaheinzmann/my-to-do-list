5ea580c02c39bff48be8db83cf8d53dabeb37986
Camila <camilapostai.ch@gmail.com>
2020-07-26 20:39:31 -0300

    add vetor, botoes exclusao e personalizacoes

--- js/main.js
+++ js/main.js
@@ -2,12 +2,53 @@ var inputElement = document.querySelector('.input-todo');
 var btnElement = document.querySelector('.btn-add');
 var ulElement = document.querySelector('.list ul');
 
-function addTodo() {
-    var listItem = document.createElement('li');
-    var textItem = document.createTextNode(inputElement.value);
+var todos = [];
 
-    listItem.appendChild(textItem);
-    ulElement.appendChild(listItem);
+function allTodos() {
+    // limpa lista para não renderizar tudo novamente
+    ulElement.innerHTML = '';
 
-    inputElement.value = "";
+    for (todo of todos) {
+        // posição da to-do no vetor
+        var pos = todos.indexOf(todo);
+
+        //cria to-do
+        var listItem = document.createElement('li');
+        var item = document.createElement('p');
+        item.setAttribute('class', 'item');
+        var textItem = document.createTextNode(todo);
+
+        //add botao excluir
+        var btnDel = document.createElement('button');
+        var textDel = document.createTextNode('Excluir');
+
+        btnDel.setAttribute('class', 'btn-del')
+        // adiciona método onclick para excluir todo na posição definida
+        btnDel.setAttribute('onclick', 'delTodo(' + pos + ')');
+
+        ulElement.appendChild(listItem);
+        listItem.appendChild(item);
+        item.appendChild(textItem);
+        listItem.appendChild(btnDel);
+        btnDel.appendChild(textDel);
+    }
 }
+
+
+function addTodo() {
+    todos.push(inputElement.value);
+    inputElement.value = "";
+    allTodos();
+}
+
+function delTodo(pos) {
+    todos.splice(pos, 1);
+    allTodos();
+}
+
+function delAll() {
+    todos = [];
+    allTodos();
+}
+
+allTodos();
