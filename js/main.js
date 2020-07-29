c5f083c8f63977290e1fbacd563e2e76a7954afe
Camila <camilapostai.ch@gmail.com>
2020-07-29 18:51:14 -0300

    add data e hora

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 var inputElement = document.querySelector('.input-todo');
 var dateElement = document.querySelector('.date-todo');
+var timeElement = document.querySelector('.time-todo');
 var btnElement = document.querySelector('.btn-add');
 var ulElement = document.querySelector('.list ul');
 
 var todos = JSON.parse(localStorage.getItem('todos_list')) || [];
+
+moment.locale('pt-br');
 
 function allTodos() {
     // limpa lista para não renderizar tudo novamente
@@ -15,21 +18,32 @@ function allTodos() {
 
         //cria to-do
         var listItem = document.createElement('li');
+        var dataItem = document.createElement('div');
+        dataItem.setAttribute('class', 'dados-item');
+
         var item = document.createElement('p');
-        item.setAttribute('class', 'item');
-        var textItem = document.createTextNode(todo);
+        item.setAttribute('class', 'item-list');
+        var textItem = document.createTextNode(todo.name);
+
+        // add data e hora
+        var dateTodo = document.createElement('span');
+        dateTodo.setAttribute('class', 'date-todo-list');
+        var textDate = document.createTextNode(todo.date + " - " + todo.time);
 
         //add botao excluir
         var btnDel = document.createElement('button');
+        btnDel.setAttribute('class', 'btn-del-list')
         var textDel = document.createTextNode('Delete');
 
-        btnDel.setAttribute('class', 'btn-del')
         // adiciona método onclick para excluir todo na posição definida
         btnDel.setAttribute('onclick', 'delTodo(' + pos + ')');
 
         ulElement.appendChild(listItem);
-        listItem.appendChild(item);
+        listItem.appendChild(dataItem);
+        dataItem.appendChild(item);
         item.appendChild(textItem);
+        dataItem.appendChild(dateTodo);
+        dateTodo.appendChild(textDate);
         listItem.appendChild(btnDel);
         btnDel.appendChild(textDel);
     }
@@ -37,8 +51,14 @@ function allTodos() {
 
 function addTodo() {
     if (inputElement.value !== '') {
-        todos.push(inputElement.value);
+        var input = {}
+        input.name = inputElement.value;
+        input.date = moment(dateElement.value).format('DD/MM/YYYY')
+        input.time = timeElement.value;
+        todos.push(input);
         inputElement.value = "";
+        dateElement.value = "";
+        timeElement.value = "";
         allTodos();
 
         saveStorage();
